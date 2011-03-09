20ae0213a7ad60be8778978f05865f42887d323e
Jack Franklin <jackfranklin375@gmail.com>
2011-03-09 00:22:45 +0000

    First commit of jQuote

---
+++ jquote.jquery.js
@@ -0,0 +1,22 @@
+jQuery.fn.jQuote = function(options) {
+
+    var defaults = {
+        quoteClass : '.quote',
+        blockQuoteClass: 'jquote-block'
+    };
+    var settings = $.extend({}, defaults, options);
+
+    return this.each(function() {
+        var $t = $(this),
+            allQuotes = $t.find(settings.quoteClass);
+        allQuotes.each(function() {
+            var $that = $(this),
+                content = $that.text(),
+                author = $that.attr("title"),
+                blockQuote = $('<blockquote/>', {
+                    "class": settings.blockQuoteClass,
+                    "html" : "<p>" + content + "<strong>" + author + "</strong></p>"
+                }).insertBefore($that);
+        });
+    });
+};
